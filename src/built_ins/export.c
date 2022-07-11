0106aec4e09f4f6d8d10a6b426c8886744922338
jrocha <jrocha@student.42wolfsburg.de>
2022-07-11 10:53:22 +0200

    merge changes

--- src/built_ins/export.c
+++ src/built_ins/export.c
@@ -6,14 +6,28 @@
 /*   By: jrocha <jrocha@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 13:47:44 by jrocha            #+#    #+#             */
-/*   Updated: 2022/07/08 14:43:15 by jrocha           ###   ########.fr       */
+/*   Updated: 2022/07/11 10:43:27 by jrocha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/minishell.h"
 
-int ms_export(char **envp, char *newvar)
+int	ms_export(t_list *env, char *newvar)
 {
-  char **tmp;
+	t_node		*node;
+	t_envvar	*line;
 
-  tmp = 
+	node = env->first;
+	if (newvar == NULL)
+	{
+		while (node)
+		{
+			line = (t_envvar *) node->data;
+			printf("declare -x ");
+			printf("%s", line->name);
+			printf("\"%s\"\n", line->value);
+			node = node->next;
+		}
+	}
+	return (0);
+}
